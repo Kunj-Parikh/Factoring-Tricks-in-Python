80944c3281bf0e7384df5507d6b4f4f36821ab46
Kunj Parikh <88554657+Kunj-Parikh@users.noreply.github.com>
2023-08-22 20:26:55 -0400

    Change program to a function
    Change program to a function and comment out the line asking for input.

--- primeFactorizer.py
+++ primeFactorizer.py
@@ -1,27 +1,35 @@
-number = int(input("What number would you like to prime factorize? "))
-if number <= 0:
-    print("Number cannot be prime factorized.")
-    exit()
-if number == 1:
-    print(1)
-currentPrime = 2
-primeList = []
-while number > 1:
-    temp = number/currentPrime
-    if temp % 1 == 0:
-        number /= currentPrime
-        primeList.append(currentPrime)
-    else:
-        currentPrime += 1
 
-uniqueFactors = list(set(primeList))
-uniqueFactors.sort()
+def primeFactorizer(number):
+    # number = int(input("What number would you like to prime factorize? "))
+    if number <= 0:
+        print("Number cannot be prime factorized.")
+        exit()
+    if number == 1:
+        print(1)
+    currentPrime = 2
+    primeList = []
+    while number > 1:
+        temp = number/currentPrime
+        if temp % 1 == 0:
+            number /= currentPrime
+            primeList.append(currentPrime)
+        else:
+            currentPrime += 1
 
-for i in range(len(uniqueFactors)):
-    power = primeList.count(uniqueFactors[i])
-    if power != 1 and i != len(uniqueFactors)-1:
-        print(f'{uniqueFactors[i]}^{power} * ', end="")
-    elif i != len(uniqueFactors)-1:
-        print(f'{uniqueFactors[i]} * ', end="")
-    else:
-        print(f'{uniqueFactors[i]}', end="")
+    uniqueFactors = list(set(primeList))
+    uniqueFactors.sort()
+    powers = []
+    for i in range(len(uniqueFactors)):
+        power = primeList.count(uniqueFactors[i])
+        powers.append(power)
+
+    for i in range(len(uniqueFactors)):
+        power = primeList.count(uniqueFactors[i])
+        if power != 1 and i != len(uniqueFactors)-1:
+            print(f'{uniqueFactors[i]}^{power} * ', end="")
+        elif i != len(uniqueFactors)-1:
+            print(f'{uniqueFactors[i]} * ', end="")
+        else:
+            print(f'{uniqueFactors[i]}', end="")
+
+primeFactorizer(40)
